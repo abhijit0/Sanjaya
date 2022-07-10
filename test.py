7ca4a4aca075a69e1aa3e81a7e1f91e05a1e0b9c
abhijit0 <ukabhijit@gmail.com>
2022-07-10 15:20:40 +0200

    Update test.py
    Removed unnecessary comments

--- test.py
+++ test.py
@@ -38,51 +38,6 @@ def predict_caption_image(model, w2i, i2w, image, n = 1, maxlen = 35):
             break
     return ' '.join([token for token in text.split() if token not in ('startseq', 'endseq')])
 
-'''def predict(model_path = './model_10.h5', data_path = './data/Flickr_Dataset/images/', encoding_path = 'test_images_encodings.pkl', test_size = 10):
-    #if(encoding_path not in os.listdir(os.getcwd())):
-    model = load_model(model_path)
-    w2i, i2w, _ = wti_itw(path = caption_token_path)
-    test_images_map = train_test_data(dataset_type='test', pkl_file_path= encoding_path , desc_path='./data/Flickr_Dataset/Flickr_TextData/Flickr_8k.testImages.txt')
-    test_images = list(test_images_map.keys())[-test_size:]
-    
-    
-    font = cv2.FONT_HERSHEY_SIMPLEX
-  
-    # org
-    org = (5, 64)
-  
-    # fontScale
-    fontScale = 0.5
-   
-    # Red color in BGR
-    color = (0, 0, 255)
-  
-    # Line thickness of 2 px
-    thickness = 2
-   
-    # Using cv2.putText() method
-    
-    plot_images = []
-    captions = []
-    for image in test_images:
-        feature_map = test_images_map[image].reshape(1,-1)
-        image_plot = cv2.imread(os.path.join(data_path, image))
-        image_resized = cv2.resize(image_plot, (512, 256))
-        cap_predicted = predict_caption_image(model, w2i, i2w, feature_map)
-        cap_array = np.zeros((128,512,3), dtype='uint8')
-        cap_image = cv2.putText(cap_array, cap_predicted, org, font, fontScale, 
-                 color, thickness, cv2.LINE_AA, False)
-        image_final = np.concatenate((image_resized, cap_image))
-        #image_final = np.append(image_resized, cap_image)
-        plot_images.append(image_final)
-        captions.append(cap_predicted)
-        
-    #cv2.namedWindow('captions', cv2.WINDOW_AUTOSIZE)
-    for image in plot_images:
-        cv2.imshow('images', image)
-        cv2.waitKey(1000)
-    cv2.destroyAllWindows()'''
-
 def window_config():
     font = cv2.FONT_HERSHEY_SIMPLEX
   
@@ -118,12 +73,7 @@ def predict(model_path = './model_10.h5', data_path = './data/Flickr_Dataset/ima
     image_final = np.concatenate((image_plot, cap_image))
     
     cv2.imwrite(f'{save_path}/{image_name}',image_final)    
-    #cv2.namedWindow('captions', cv2.WINDOW_AUTOSIZE)
-    #cv2.imshow('images', image_final)
-    #cv2.waitKey(10)
-    #cv2.destroyAllWindows()
     
-
 def cap_similarity(cap1, cap2):
     cap1_vector = nlp(cap1).vector
     cap2_vector = nlp(cap2).vector
@@ -170,9 +120,7 @@ def predict_video(model_path = './model_10.h5', video_path = './data/test_videos
         else:
             break
     cap.release()
-    
-    #print(frames[0].shape)
-    #print((frames[0].shape[0],frames[0].shape[1]))
+
     result = cv2.VideoWriter(f'{save_path}/{video_name[:-4]}.avi', 
                          cv2.VideoWriter_fourcc(*'MJPG'),
                          10, (frames[0].shape[1],frames[0].shape[0]))
@@ -182,17 +130,6 @@ def predict_video(model_path = './model_10.h5', video_path = './data/test_videos
         result.write(frame)
         cv2.waitKey(30)
     
-    '''for caption,frame in zip(captions_audio, frames):
-        if(captions_audio.index(caption) > 0 and captions_audio.index(caption) < len(captions_audio)):
-            prev_caption = captions_audio[captions_audio.index(caption) - 1]
-            if(caption != prev_caption):
-                play_audio(caption)
-            
-        cv2.imshow('Frame', frame)
-        cv2.waitKey(50)
-            
-
-    cv2.destroyAllWindows()'''
 
 def play_audio(text = 'Hello there'):
     mp3_fp = '/tmp/temp.wav'
